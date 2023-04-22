a7cabe2583d3ba557d0a1292fc75e76cee9e7fdd
Jacek <jpajak@yandex.com>
2023-04-22 12:29:55 +0200

    testNavigatorClassExists - test fails

--- src/test/java/org/mars/rover/kata/KataTest.java
+++ src/test/java/org/mars/rover/kata/KataTest.java
@@ -2,11 +2,26 @@ package org.mars.rover.kata;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.*;
 
 public class KataTest {
     @Test
     public void testRoverClassExists() {
         String className = "org.mars.rover.kata.MarsRover";
+
+        try {
+            Class<?> myClass = Class.forName(className);
+            Assertions.assertNotNull(myClass);
+            Assertions.assertEquals(className, myClass.getName());
+            System.out.println("Class " + className + " exists.");
+        } catch (ClassNotFoundException e) {
+            Assertions.fail("Class " + className + " does not exist.");
+        }
+    }
+
+    @Test
+    public void testNavigatorClassExists() {
+        String className = "org.mars.rover.kata.MarsNavigator";
 
         try {
             Class<?> myClass = Class.forName(className);
@@ -29,4 +44,6 @@ public class KataTest {
         Assertions.assertEquals(marsRover.getPosition().getX(), 0);
         Assertions.assertEquals(marsRover.getPosition().getY(), 1);
     }
+
+
 }
