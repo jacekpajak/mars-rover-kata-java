93c2ddd7642cdec1f72b972a27ef1048fb48706a
Jacek <jpajak@yandex.com>
2023-04-22 13:31:16 +0200

    testNavigatorClassExists - test passes

--- src/test/java/org/mars/rover/kata/KataTest.java
+++ src/test/java/org/mars/rover/kata/KataTest.java
@@ -1,27 +1,21 @@
 package org.mars.rover.kata;
 
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.util.*;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
 
+@RunWith(MockitoJUnitRunner.class)
 public class KataTest {
+    @Rule
+    public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+
     @Test
     public void testRoverClassExists() {
         String className = "org.mars.rover.kata.MarsRover";
-
-        try {
-            Class<?> myClass = Class.forName(className);
-            Assertions.assertNotNull(myClass);
-            Assertions.assertEquals(className, myClass.getName());
-            System.out.println("Class " + className + " exists.");
-        } catch (ClassNotFoundException e) {
-            Assertions.fail("Class " + className + " does not exist.");
-        }
-    }
-
-    @Test
-    public void testNavigatorClassExists() {
-        String className = "org.mars.rover.kata.MarsNavigator";
 
         try {
             Class<?> myClass = Class.forName(className);
@@ -45,5 +39,18 @@ public class KataTest {
         Assertions.assertEquals(marsRover.getPosition().getY(), 1);
     }
 
+    @Test
+    public void testNavigatorClassExists() {
+        String className = "org.mars.rover.kata.MarsNavigator";
+
+        try {
+            Class<?> myClass = Class.forName(className);
+            Assertions.assertNotNull(myClass);
+            Assertions.assertEquals(className, myClass.getName());
+            System.out.println("Class " + className + " exists.");
+        } catch (ClassNotFoundException e) {
+            Assertions.fail("Class " + className + " does not exist.");
+        }
+    }
 
 }
